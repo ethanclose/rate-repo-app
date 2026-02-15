0306080abe8b8d7cd05be5b7780fefb825b2b304
Ethan Close <ethan.close@gmail.com>
2026-02-15 11:38:00 -0800

    bigger test working

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,5 +1,23 @@
-describe('Example', () => {
-  it('works', () => {
-    expect(1).toBe(1);
+import React from 'react';
+import { Text, View } from 'react-native';
+import { render, screen } from '@testing-library/react-native';
+
+const Greeting = ({ name }) => {
+  return (
+    <View>
+      <Text>Hello {name}!</Text>
+    </View>
+  );
+};
+
+describe('Greeting', () => {
+  it('renders the correct message based on props', () => {
+    render(<Greeting name="Ethan" />);
+
+    screen.debug();
+
+    expect(screen.getByText('Hello Ethan!')).toBeDefined();
+
+    expect(screen.getByText('Hello Ethan!')).toBeOnTheScreen();
   });
 });
