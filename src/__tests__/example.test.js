0c9ded24b799446c87b5446c991667887842a51a
Ethan Close <ethan.close@gmail.com>
2026-02-15 11:22:03 -0800

    reverting to original test

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react-native';
-import { View, Text } from 'react-native';
-
 describe('Example', () => {
   it('works', () => {
-    render(
-      <View>
-        <Text>Hello World</Text>
-      </View>,
-    );
-    expect(screen.getByText('Hello World')).toBeOnTheScreen();
+    expect(1).toBe(1);
   });
 });
